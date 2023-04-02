45b5efa30fe932bd2b268372690c9993f37f33bc
fatimasouliman <fffaat.sss@gmail.com>
2023-04-03 00:51:27 +0300

    first commit

---
+++ assest/javascript/home.js
@@ -0,0 +1,21 @@
+let choices= document.querySelectorAll(".choices li");
+let cards= document.querySelectorAll(".cards .card");
+choices.forEach((li) => {
+    li.addEventListener("click",removeActive);
+    li.addEventListener("click",remove);
+});
+function removeActive() {
+    choices.forEach((li) => {
+        li.classList.remove("active");
+        this.classList.add("active");
+    });
+}
+function remove() {
+    cards.forEach((card) => {
+        card.style.visibility="hidden";
+    });
+    document.querySelectorAll(this.dataset.cat).forEach((el) => {
+        el.style.visibility="visible";
+    })
+    
+}
